7e38471135cc86e7f806cf5f15d1cd83a8265889
Og-Brutal <ogbrutal2825@gmail.com>
2026-06-22 20:24:10 +0500

    Add SQL query solution for LeetCode 184: Department Highest Salary

---
+++ Database_Questions/184_Department_Highest_Salary/solution.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT D.name AS Department, E.name AS Employee, E.salary AS Salary 
+FROM Employee AS E
+JOIN Department AS D ON E.departmentId = D.id 
+WHERE E.salary = (
+    SELECT MAX(salary) 
+    FROM Employee 
+    WHERE departmentId = E.departmentId
+);
