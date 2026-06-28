f93f4932c0919b6284947ef99791351b1a19a077
Og-Brutal <ogbrutal2825@gmail.com>
2026-06-28 19:44:35 +0500

    Add JavaScript solution for LeetCode 2620: Counter

---
+++ JavaScript_Questions/2620_Counter/solution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = 0;
+    return function() {
+        if (count === 0) {
+            count++;
+            return n;
+        } else {
+            return ++n;
+        }
+    };
+};
+
+/**
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
