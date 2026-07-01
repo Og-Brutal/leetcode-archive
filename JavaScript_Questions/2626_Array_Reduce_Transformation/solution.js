ae6ed35a3a61aa369a6ead1c6e484343b797a0fb
Og-Brutal <ogbrutal2825@gmail.com>
2026-07-01 21:48:19 +0500

    Add JavaScript solution for LeetCode 2626: Array Reduce Transformation

---
+++ JavaScript_Questions/2626_Array_Reduce_Transformation/solution.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if (nums.length === 0) return init;
+    let val = init;
+    nums.forEach((num) => { val = fn(val, num); });
+    return val;
+};
